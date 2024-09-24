4ca36102758ffa6667d458136c90c4b1874e090c
andre <andrefpbraga@gmail.com>
2024-09-23 23:23:34 -0300

    Finalização do app para consultar tabuada.

--- app/src/main/java/com/example/tabooada/MainActivity.kt
+++ app/src/main/java/com/example/tabooada/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.tabooada
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
 import com.example.tabooada.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -30,11 +33,11 @@ class MainActivity : AppCompatActivity() {
         binding.spinnerMultiplies.adapter = adapter
 
 
-        var tabuadaSelecionada = 0
+        var tabuadaSelecionada = 1
         binding.spinnerMultiplies.onItemSelectedListener =
             object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                    tabuadaSelecionada = p2
+                    tabuadaSelecionada = p2 + 1
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -42,8 +45,21 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+        binding.btnLimpar.setOnClickListener {
+            binding.llResultadoLayout.removeAllViews()
+        }
         binding.btnConsultarTabuada.setOnClickListener {
+            for (i in 1..10) {
+                val resultado = tabuadaSelecionada * i
+                val textView = TextView(this)
+                textView.text = "$tabuadaSelecionada x $i = $resultado"
+                textView.textSize = 24f
+                textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+                textView.setTypeface(null, Typeface.BOLD)
+                textView.setPadding(0, 8, 0, 8)
 
+                binding.llResultadoLayout.addView(textView)
+            }
         }
     }
 }
